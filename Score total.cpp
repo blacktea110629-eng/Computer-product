cb85a89b0112b7c75378ed3f914592cd78b4207a
GB_1437 <blacktea110629@gmail.com>
2026-09-15 11:16:40 +0800

    Add files via upload

---
+++ Score total.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+    int main()
+    {
+        int Total,ChineseScore,EnglishScore;
+        cin >> ChineseScore >> EnglishScore;
+        Total =ChineseScore+EnglishScore;
+        cout << "Total=" << Total << endl ;
+        return 0;1
+    }
